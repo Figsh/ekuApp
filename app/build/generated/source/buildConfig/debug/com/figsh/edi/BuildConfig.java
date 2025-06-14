0034e22880d997c7616039dd83031acd2ca6d20f
Figsh <safetyunion5550@gmail.com>
2025-06-14 13:12:45 +0100

    Update and rename BuildConfig.java to BuildConfig.java

--- app/build/generated/source/buildConfig/debug/com/figsh/edi/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/figsh/edi/BuildConfig.java
@@ -1,7 +1,7 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.yoursite.app;
+package com.figsh.edi;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
